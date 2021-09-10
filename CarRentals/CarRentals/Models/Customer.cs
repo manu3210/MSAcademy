5342a01c9bd3cc107adc23b9e21fc6fac9946f44
Emanuel Rivas <manu_1019@hotmail.com>
2021-09-10 14:41:08 -0300

    Models Customer and Rental were created

---
+++ CarRentals/CarRentals/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarRentals.Models
+{
+    class Customer
+    {
+        string Dni { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Phone { get; set; }
+        string Adress { get; set; }
+        string City { get; set; }
+        string Province { get; set; }
+        int ZipCode { get; set; }
+        DateTime LastModification { get; set; }
+    }
+}
---
+++ CarRentals/CarRentals/Models/Rental.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+
+namespace CarRentals.Models
+{
+    class Rental
+    {
+        DateTime Beginning { get; set; }
+        DateTime End { get; set; }
+        Customer Customer { get; set; }
+        Car Car { get; set; }
+    }
+}
