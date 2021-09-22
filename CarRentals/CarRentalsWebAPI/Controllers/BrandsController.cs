d667b6761818a553073aa85f36e8098ffc130b8a
Emanuel Rivas <manu_1019@hotmail.com>
2021-09-21 21:50:52 -0300

    Controllers were done

---
+++ CarRentals/CarRentalsWebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,103 @@
+﻿using CarRentalsWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalsWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly CarRentalsContext _context;
+
+        public BrandsController(CarRentalsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Brands
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            return await _context.Brands.ToListAsync();
+        }
+
+        // GET: api/Brands/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Brand>> GetBrand(int id)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return brand;
+        }
+
+        // PUT: api/Brands/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
+        {
+            if (id != brand.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(brand).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BrandExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Brands
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBrand", new { id = brand.Id }, brand);
+        }
+
+        // DELETE: api/Brands/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            _context.Brands.Remove(brand);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BrandExists(int id)
+        {
+            return _context.Brands.Any(e => e.Id == id);
+        }
+    }
+}
